822a55e66c003570913f74d106b0519d21994f1a
Long <julyzergcn1@gmail.com>
2012-07-28 07:57:51 +0800

    update csrf_get app

--- csrf_get/middleware.py
+++ csrf_get/middleware.py
@@ -2,11 +2,43 @@ import urlparse, urllib
 from django.http import HttpResponseRedirect
 
 class CsrfMiddleware(object):
-    
+    '''
+    Detect "csrfmiddlewaretoken" in the request string, and if it is there then remove it and redirect to the new version without that parameter. For example:
+
+    /foo?a=1&b=2&csrfmiddlewaretoken=foo&d=4
+
+    should redirect to
+
+    /foo?a=1&b=2&d=4
+    '''
     def process_request(self, request):
-        if request.method=='GET' and request.GET.has_key('csrfmiddlewaretoken'):
-            parse_result = list(urlparse.urlparse(request.get_full_path()))
-            parse_result[4] = urllib.urlencode([tu for tu in urlparse.parse_qsl(parse_result[4]) if tu[0]!='csrfmiddlewaretoken'])
+        if request.method=='GET' and \
+            request.GET.has_key('csrfmiddlewaretoken'):
+            request_full_path = request.get_full_path()
+            parse_result = urlparse.urlparse(request_full_path)
+            
+            # parse_result is a tuple ParseResult(scheme, netloc, path, params, query, fragment)
+            # convert the tuple to a list
+            parse_result = list(parse_result)
+            
+            # the 4th of the list should be the query string, 
+            # e.g. "a=1&b=2&csrfmiddlewaretoken=foo&d=4"
+            query_string = parse_result[4]
+            
+            # parse the query string to a tuple list of (key, value),
+            # e.g. [('a', '1'), ('b', '2'), ('csrfmiddlewaretoken', 'foo'), ('d', '4')]
+            query_list = urlparse.parse_qsl(query_string)
+            
+            # filter out the 'csrfmiddlewaretoken' tuple from the list
+            query_list = [tu for tu in query_list if tu[0]!='csrfmiddlewaretoken']
+            
+            # convert the new tuple list to a query string
+            query_string = urllib.urlencode(query_list)
+            
+            parse_result[4] = query_string
+            
+            # conver the ParseResult tuple to a new path
             new_full_path = urlparse.urlunparse(parse_result)
+            
             return HttpResponseRedirect(new_full_path)
     
--- csrf_get/tests.py
+++ csrf_get/tests.py
@@ -2,8 +2,20 @@ from django.test import TestCase, client
 import urlparse
 
 class CsrfGetTest(TestCase):
-    
+    '''
+    Detect "csrfmiddlewaretoken" in the request string, and if it is there then remove it and redirect to the new version without that parameter. For example:
+
+    /foo?a=1&b=2&csrfmiddlewaretoken=foo&d=4
+
+    should redirect to
+
+    /foo?a=1&b=2&d=4
+    '''
     def test_csrf_get(self):
         c = client.Client()
-        response = c.get('/some/path/?csrfmiddlewaretoken=a&*2;b&aa=22&bb=33')
-        self.assertEqual(urlparse.urlparse(response.get('location')).query, urlparse.urlparse('/some/path/?aa=22&bb=33').query)
+        response = c.get('/foo?a=1&b=2&csrfmiddlewaretoken=foo&d=4')
+        location_url = response.get('location')
+        query_string = urlparse.urlparse(location_url).query
+        
+        test_query_string = urlparse.urlparse('/foo?a=1&b=2&d=4').query
+        self.assertEqual(query_string, test_query_string)
